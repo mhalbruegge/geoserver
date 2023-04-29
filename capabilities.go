7b95cdb6c2d5cbf1d9a4b89b4acf6fab09574aa6
mhalbruegge <mhalbruegge@uni-osnabrueck.de>
2023-04-29 17:25:22 +0200

    Change module repository

--- capabilities.go
+++ capabilities.go
@@ -1,7 +1,7 @@
 package geoserver
 
 import (
-	"github.com/hishamkaram/geoserver/wms"
+	"github.com/mhalbruegge/geoserver/wms"
 )
 
 //GetCapabilities Retrieves metadata about the service, including supported operations and parameters,
